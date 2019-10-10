8476b6ec7fcb1036e8933a50882694624e2192c2
CarbonEternity <pa03622789@gmail.com>
2019-10-10 11:12:28 +0300

    add rounded-circle avatar

--- html/MyPage.php
+++ html/MyPage.php
@@ -25,8 +25,7 @@ require "../connect/connectBD.php";
 $sql = "SELECT image2 FROM users2 where email='$email' and password='$pass' ";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
-echo "<img src='../uploaded/$row[image2]' />";
+echo "<img class='size-5 md-avatar img-thumbnail rounded-circle float-right' alt='Cinque Terre' src='../uploaded/$row[image2]'  />";
 ?>
-
 </body>
 </html>
